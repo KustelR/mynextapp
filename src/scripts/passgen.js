2b68d4b2b83e77437212d3228cf2cca0491a80d4
KustelR <dmitriysstudios2@gmail.com>
2023-10-15 21:35:23 +0400

    Client-side hashing simplified

--- src/scripts/passgen.js
+++ src/scripts/passgen.js
@@ -3,7 +3,8 @@ import CryptoJS from "crypto-js";
 
 async function generatePasswordHash(password, salt) {
     return await new Promise((resolve) => {
-        resolve(CryptoJS.PBKDF2(password, salt, {keySize: 32, iterations: 20000}).toString())
+        const hash = CryptoJS.PBKDF2(password, salt, {keySize: 32, iterations: 1}).toString();
+        resolve(hash);
     })
 }
 

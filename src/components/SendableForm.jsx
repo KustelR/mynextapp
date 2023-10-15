db65ee07072c852ef3a26e445eee729d2dbe0548
KustelR <dmitriysstudios2@gmail.com>
2023-10-15 16:11:38 +0400

    Sendable form decomposition

--- src/components/SendableForm.jsx
+++ src/components/SendableForm.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, {useState} from 'react';
-import axios from 'axios';
 
 import ShowIf from './ui/ShowIf';
 import SuccessBox from './ui/popups/SuccessBox';
@@ -9,40 +8,7 @@ import ErrorBox from './ui/popups/ErrorBox';
 import PendingBox from './ui/popups/PendingBox';
 
 
-function createDataObject(element) {
-    let data = {};
-    const form = Object.entries(element.target);
-    form.forEach(item => {
-        const tagName = item[1].tagName;
-        const id = item[1].id;
-        const value = item[1].value;
-        if (tagName && id && value) {
-            data[id] = value;
-        }
-    });
-    return data;
-}
-
-function clearForm(element) {
-    const form = Object.entries(element.target);
-    let i = 0;
-    form.forEach(item => {
-        if (item[1].value) {
-            element.target[item[0]].value = '';
-        }
-    })
-}
-
-async function sendFormData(url, element, callback) {
-    const data = createDataObject(element);
-
-    await axios.post(url, data, {headers: "application/json"})
-        .then(response => {callback(response, null); clearForm(element);})
-        .catch(err => {callback(null, err);});
-}
-
-
-export default function SendableForm({href, children}) {
+export default function SendableForm({href, children, submitHandler}) {
 
     const [isFailed, setIsFailed] = useState();
     const [isLoading, setIsLoading] = useState();
@@ -73,7 +39,7 @@ export default function SendableForm({href, children}) {
     }
 
   return (
-    <form onSubmit={e => {e.preventDefault(); sendFormData(href, e, handleSendResult); setIsLoading(true); setIsFailed(undefined)}}>
+    <form onSubmit={e => {e.preventDefault(); submitHandler(href, e, handleSendResult); setIsLoading(true); setIsFailed(undefined)}}>
         <ShowIf 
             isVisible={!isFailed && (isFailed !== undefined)}>
             <SuccessBox title={infoBox.title} body={infoBox.body} />

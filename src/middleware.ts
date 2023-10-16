6e9898778455381751ecb07eef5c5fd81b77d9be
KustelR <dmitriysstudios2@gmail.com>
2023-10-16 23:07:32 +0400

    Middleware query params support

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,9 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL(process.env.API_HOST + request.nextUrl.pathname, request.url))
+  const pathname = request.nextUrl.pathname;
+  const searchParams = request.nextUrl.searchParams;
+  return NextResponse.redirect(new URL(process.env.API_HOST + pathname + "?" + searchParams, request.url))
 }
  
 // See "Matching Paths" below to learn more

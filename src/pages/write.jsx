d01e4f10d9076fb2f43e19208d896d3d3351ec02
KustelR <dmitriysstudios2@gmail.com>
2023-10-18 23:06:12 +0400

    Page for article writing

---
+++ src/pages/write.jsx
@@ -0,0 +1,12 @@
+import '../app/globals.css'
+
+import ArticleWrite from '@/components/ArticleWrite'
+import PageComponent from '@/components/spalike/PageComponent'
+
+export default function Home() {
+    return (
+    <PageComponent>
+        <ArticleWrite></ArticleWrite>
+    </PageComponent>
+    )
+}

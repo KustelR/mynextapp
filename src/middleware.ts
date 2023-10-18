1c98c4b226f996ce4fe1faab06c520b33216e4ad
KustelR <dmitriysstudios2@gmail.com>
2023-10-18 22:58:26 +0400

    Fixed search params in middleware (again)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  const searchParams = request.nextUrl.searchParams ? "" : "?" + request.nextUrl.searchParams;
+  const searchParams = ((request.nextUrl.searchParams + "") ? "?" + request.nextUrl.searchParams : "");
   return NextResponse.redirect(new URL(process.env.API_HOST + pathname + searchParams, request.url))
 }
  

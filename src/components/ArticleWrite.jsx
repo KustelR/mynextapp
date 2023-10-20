7c3133aa49e9b3193da397d90965051cf6cac623
KustelR <dmitriysstudios2@gmail.com>
2023-10-21 00:58:57 +0400

    More appropriate formatting

--- src/components/ArticleWrite.jsx
+++ src/components/ArticleWrite.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import SendableForm from './SendableForm'
-import CustomInput from './ui/CustomInput'
-import CustomTextArea from './ui/CustomTextArea'
+import CustomInput from './ui/inputs/CustomInput'
+import CustomTextArea from '@/components/ui/inputs/CustomTextArea'
 import TextButton from './ui/TextButton'
 import { sendFormData } from '@/scripts/forms/sendArticle'
+import RichTextEditor from '@/components/ui/inputs/RichTextEditor'
 
-export default function ArticleWrite() {
+export default function ArticleWrite({className}) {
+
+  const [articleBody, setArticleBody] = useState();
+
   return (
-    <div className='container md:shadow-lg max-w-screen-md mx-auto p-4 bg-white dark:bg-neutral-800'>
+    <div className={'container md:shadow-lg max-w-screen-md mx-auto p-4 ' + className}>
         <SendableForm 
             href="/api/v1/articles/create"
-            submitHandler={sendFormData}>
+            submitHandler={(url, element, callback) => {sendFormData(url, element, callback, [["body", articleBody], ["accessToken", localStorage.getItem('accessToken')]])}}>
+            <CustomInput
+              label="Title" 
+              id="title" 
+              placeholder="Your title..."
+              className="mb-4" />
+              <RichTextEditor 
+                className="mb-4 min-h-screen" 
+                label="Article body" 
+                onChange={(e) => {setArticleBody(e)}}/>
+              <CustomTextArea 
+                className="mb-4" 
+                label="Short description" 
+                id="description" />
             <CustomInput 
-                id="title" 
-                placeholder="Your title..."
-                className="mb-4" />
-            <CustomTextArea id="body" placeholder="Your text here..."></CustomTextArea>
-            <CustomInput label="Tags" id="tags" placeholder="text" className="mb-4"></CustomInput>
+              autoComplete="off" 
+              label="Tags" 
+              id="tags" 
+              placeholder="text" 
+              className="mb-4" />
             <TextButton>POST ARTICLE</TextButton>
             </SendableForm>
         </div>

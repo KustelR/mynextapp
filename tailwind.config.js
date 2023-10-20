d0b755c8f1618174f54b4498f6580fd6db8d5b1b
KustelR <dmitriysstudios2@gmail.com>
2023-10-21 00:59:35 +0400

    New custom min height

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      minHeight: {
+        '200px': '200px',
+      },
       minWidth: {
         '1/2': '50%',
         '8/10': '80%'

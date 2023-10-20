61b56715c2bd84ea0206a0f0b78342412aded905
KustelR <dmitriysstudios2@gmail.com>
2023-10-21 01:02:33 +0400

    No more errors when provided empty additional data

--- src/scripts/forms/sendArticle.js
+++ src/scripts/forms/sendArticle.js
@@ -3,11 +3,15 @@ import createDataObject from './createObjectFromForm';
 import clearForm from './clearForm';
 
 
-async function sendFormData(url, element, callback) {
-    const data = createDataObject(element);
-
+async function sendFormData(url, element, callback, additionalData) {
+    let data = createDataObject(element);
+    for (let i = 0; i < additionalData.length; i++) {
+        console.log(additionalData[i]);
+        data[additionalData[i][0]] = additionalData[i][1];
+    }
+    console.log(data);
+    if (!data.body) throw new Error('No article provided!'); 
     const body = data.body.split('\n');
-    console.log(body);
     let parsedBody = body.forEach((paragraph) => {paragraph.append})
 
     await axios.post(url, data, {headers: "application/json"})

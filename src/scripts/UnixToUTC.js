d4df3276b18266f2fd5871d8421d34f32d693a00
KustelR <dmitriysstudios2@gmail.com>
2023-10-24 20:42:18 +0400

    Time conversion in dedicated file

---
+++ src/scripts/UnixToUTC.js
@@ -0,0 +1,12 @@
+export default function UnixToUTC(time) {
+    const date = new Date(time);
+
+    const minutes = date.getMinutes().toString();
+    const hours = date.getHours().toString();
+    const day = date.getDate().toString();
+    const month = date.getMonth().toString();
+    const year = date.getFullYear().toString();
+
+    return `${hours.length > 1 ? hours : "0" + hours}:${minutes.length > 1 ? minutes : "0" + minutes} 
+            ${day}.${month}.${year}`
+}

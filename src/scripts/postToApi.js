1d90b2aeadcc73f93c0dbd03827332b1c3713b75
KustelR <dmitriysstudios2@gmail.com>
2023-10-28 13:45:54 +0400

    JSDoc changes

--- src/scripts/postToApi.js
+++ src/scripts/postToApi.js
@@ -5,9 +5,9 @@ import axios from "axios";
  * Sends data to specified url
  * @param {String} url Target url
  * @param {Object} payload Data to be sent in body
- * @param {Object} params Query params
+ * @param {Object} headers Sets headers to be sent in request
  * @returns {Promise<AxoisResponse>} response from server
  */
-export default function post(url, payload, params) {
-    return axios.post(url, payload, {params: params})
+export default function post(url, payload, headers) {
+    return axios.post(url, payload, {headers: headers})
 }

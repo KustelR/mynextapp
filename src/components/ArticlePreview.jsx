84b2d1553f5e3601ac54136e32ea8aa1bb635447
KustelR <dmitriysstudios2@gmail.com>
2023-10-28 12:42:45 +0400

    Dark hover color lighntened

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -8,7 +8,7 @@ import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 export default function ArticlePreview({article}) {
   return (
     <Link href={"/articles/read?_id=" + article._id}>
-        <div className='w-full hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 mb-2'>
+        <div className='w-full hover:bg-neutral-100 dark:hover:bg-neutral-700 p-2 mb-2'>
             <div className='md:flex'>
                 <h3 className='font-bold mr-2'>{article.title}</h3>
                 <div className='opacity-60'>
@@ -20,7 +20,7 @@ export default function ArticlePreview({article}) {
             <div>
             <strong>Tags: </strong>
                 <ul>
-                    <li>{article.tags.map((tag) => {return (<span key={tag} className='mr-1 p-1 bg-neutral-300 dark:bg-neutral-500 dark:rounded-lg'>{tag}</span>)})}</li>
+                    <li>{article.tags.map((tag) => {return (<span key={tag} className='mr-1 p-1 bg-neutral-300 dark:bg-neutral-500'>{tag}</span>)})}</li>
                 </ul>
             </div>
             <div>

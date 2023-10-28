caf2cfee6854cd14be69621547deef7944e2fcea
KustelR <dmitriysstudios2@gmail.com>
2023-10-28 20:29:46 +0400

    Time display error fixed

--- src/scripts/UnixToUTC.js
+++ src/scripts/UnixToUTC.js
@@ -4,7 +4,7 @@ export default function UnixToUTC(time) {
     const minutes = date.getMinutes().toString();
     const hours = date.getHours().toString();
     const day = date.getDate().toString();
-    const month = date.getMonth().toString();
+    const month = (date.getMonth() + 1).toString();
     const year = date.getFullYear().toString();
 
     return `${hours.length > 1 ? hours : "0" + hours}:${minutes.length > 1 ? minutes : "0" + minutes} 

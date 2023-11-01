02a7198eef935f054da6eee58ab8dba2e2bbd2af
KustelR <dmitriysstudios2@gmail.com>
2023-11-01 21:40:32 +0400

    Support for v2 api

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,16 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const searchParams = ((request.nextUrl.searchParams + "") ? "?" + request.nextUrl.searchParams : "");
-  return NextResponse.redirect(new URL(process.env.API_HOST + pathname + searchParams, request.url))
+  const apiVersion = request.nextUrl.pathname.slice(5,8);
+  if (apiVersion === "v2") {
+    return NextResponse.redirect(new URL(process.env.API_HOST + pathname + searchParams, request.url))
+  }
+  else {
+    return NextResponse.redirect(new URL(process.env.API_HOST_LEGACY + pathname + searchParams, request.url))
+  }
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/api/v1/:path*', '/auth/v1/:path*'],
+  matcher: ['/api/v1/:path*', '/auth/v1/:path*', '/api/v2/:path*', '/auth/v2/:path*'],
 }

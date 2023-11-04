ba56fce8cdb6ce183950d5621de56c5bc5dbc1ba
KustelR <dmitriysstudios2@gmail.com>
2023-11-04 22:18:02 +0400

    New styling feauters

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#e63946'
+        primary: '#e63946',
+        secondary: '#1D3557',
+        "secondary-light": '#457B9D',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      maxHeight: {
+        '800px': '800px'
       },
       minHeight: {
         '200px': '200px',
@@ -25,9 +30,11 @@ module.exports = {
       },
       maxWidth: {
         '30ch': '30ch',
+        '10ch': '10ch',
         '8/10': '80%'
       },
       minWidth: {
+        '30ch': '30ch',
         '1/2': '50%',
         '8/10': '80%'
       },

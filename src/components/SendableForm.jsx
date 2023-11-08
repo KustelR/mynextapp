85d3366e133fd7635c4fa142155e4272a29e2c02
KustelR <dmitriysstudios2@gmail.com>
2023-11-08 22:05:42 +0400

    Prettier'd

--- src/components/SendableForm.jsx
+++ src/components/SendableForm.jsx
@@ -17,70 +17,70 @@ import PendingBox from "./ui/popups/PendingBox";
  * @returns {React.JSX.Element}
  */
 export default function SendableForm({ href, children, submitHandler }) {
-  const [isFailed, setIsFailed] = useState();
-  const [isLoading, setIsLoading] = useState();
-  const [infoBox, setInfoBox] = useState({});
+    const [isFailed, setIsFailed] = useState();
+    const [isLoading, setIsLoading] = useState();
+    const [infoBox, setInfoBox] = useState({});
 
-  async function submit(e) {
-    e.preventDefault();
-    if (submitHandler) {
-      submitHandler(href, e, handleSendResult);
-      setIsLoading(true);
-      setIsFailed(undefined);
-    } else {
-      console.error("No submit handler provided to the form");
-    }
-  }
-
-  async function handleSendResult(response, error) {
-    error // This cursed abomination assigns error.response to response if there is no response, but error.response
-      ? error.response
-        ? (response = error.response)
-        : (response = null)
-      : (error = null);
-    let data;
-    if (response) {
-      if (response.status.toString().startsWith("2")) {
-        data = response.data;
-        setIsFailed(false);
-        setInfoBox({ title: data.messageTitle, body: data.message });
-      } else {
-        data = response.data;
-        setIsFailed(true);
-        setInfoBox({ title: data.messageTitle, body: data.message });
-      }
-    } else {
-      setIsFailed(true);
-      setInfoBox({ title: "Form sending failed", body: error.message });
-    }
-
-    if (data) {
-      if (data.toLocalStorage) {
-        const keys = Object.keys(data.toLocalStorage);
-        for (let i = 0; i < keys.length; i++) {
-          localStorage.setItem(keys[i], data.toLocalStorage[keys[i]]);
+    async function submit(e) {
+        e.preventDefault();
+        if (submitHandler) {
+            submitHandler(href, e, handleSendResult);
+            setIsLoading(true);
+            setIsFailed(undefined);
+        } else {
+            console.error("No submit handler provided to the form");
         }
-      }
     }
 
-    setIsLoading(false);
-  }
+    async function handleSendResult(response, error) {
+        error // This cursed abomination assigns error.response to response if there is no response, but error.response
+            ? error.response
+                ? (response = error.response)
+                : (response = null)
+            : (error = null);
+        let data;
+        if (response) {
+            if (response.status.toString().startsWith("2")) {
+                data = response.data;
+                setIsFailed(false);
+                setInfoBox({ title: data.messageTitle, body: data.message });
+            } else {
+                data = response.data;
+                setIsFailed(true);
+                setInfoBox({ title: data.messageTitle, body: data.message });
+            }
+        } else {
+            setIsFailed(true);
+            setInfoBox({ title: "Form sending failed", body: error.message });
+        }
 
-  return (
-    <form onSubmit={submit}>
-      <ShowIf isVisible={!isFailed && isFailed !== undefined}>
-        <SuccessBox title={infoBox.title} body={infoBox.body} />
-      </ShowIf>
-      <ShowIf isVisible={isFailed}>
-        <ErrorBox title={infoBox.title} body={infoBox.body} />
-      </ShowIf>
-      <ShowIf isVisible={isLoading}>
-        <PendingBox
-          title="Sending form to the server..."
-          body="Please stand by"
-        />
-      </ShowIf>
-      {children}
-    </form>
-  );
+        if (data) {
+            if (data.toLocalStorage) {
+                const keys = Object.keys(data.toLocalStorage);
+                for (let i = 0; i < keys.length; i++) {
+                    localStorage.setItem(keys[i], data.toLocalStorage[keys[i]]);
+                }
+            }
+        }
+
+        setIsLoading(false);
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <ShowIf isVisible={!isFailed && isFailed !== undefined}>
+                <SuccessBox title={infoBox.title} body={infoBox.body} />
+            </ShowIf>
+            <ShowIf isVisible={isFailed}>
+                <ErrorBox title={infoBox.title} body={infoBox.body} />
+            </ShowIf>
+            <ShowIf isVisible={isLoading}>
+                <PendingBox
+                    title="Sending form to the server..."
+                    body="Please stand by"
+                />
+            </ShowIf>
+            {children}
+        </form>
+    );
 }
--- src/components/views/ArticleWrite.jsx
+++ src/components/views/ArticleWrite.jsx
@@ -1,60 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
 
 import SendableForm from "../SendableForm";
 import CustomInput from "../ui/inputs/CustomInput";
 import CustomTextArea from "@/components/ui/inputs/CustomTextArea";
 import TextButton from "../ui/TextButton";
 import { sendFormData } from "@/scripts/forms/sendArticle";
+import patchData from "@/scripts/forms/patchArticle";
 import RichTextEditor from "@/components/ui/inputs/RichTextEditor";
 
 export default function ArticleWrite({ className }) {
-  const [articleBody, setArticleBody] = useState();
+    const [articleBody, setArticleBody] = useState();
+    const title = useRef(null);
+    const description = useRef(null);
+    const tags = useRef(null);
+    const [isUpdate, setIsUpdate] = useState(false);
 
-  return (
-    <div
-      className={
-        "container md:shadow-lg max-w-screen-md mx-auto p-4 " + className
-      }
-    >
-      <SendableForm
-        href="/api/v1/articles"
-        submitHandler={(url, element, callback) => {
-          sendFormData(
-            url,
-            element,
-            callback,
-            { body: articleBody },
-            { "x-access-token": localStorage.getItem("accessToken") }
-          );
-        }}
-      >
-        <CustomInput
-          label="Title"
-          id="title"
-          placeholder="Your title..."
-          className="mb-4 text-2xl"
-        />
-        <RichTextEditor
-          className="mb-4 min-h-500px"
-          label="Article body"
-          onChange={(e) => {
-            setArticleBody(e);
-          }}
-        />
-        <CustomTextArea
-          className="mb-4"
-          label="Short description"
-          id="description"
-        />
-        <CustomInput
-          autoComplete="off"
-          label="Tags"
-          id="tags"
-          placeholder="text"
-          className="mb-4"
-        />
-        <TextButton>POST ARTICLE</TextButton>
-      </SendableForm>
-    </div>
-  );
+    let searchParams = useSearchParams();
+    useEffect(() => {
+        async function loadArticle() {
+            const article = (await axios.get(`/api/v2/articles/?_id=${id}`))
+                .data;
+            console.log(article);
+            title.current.value = article.title;
+            description.current.value = article.description;
+            tags.current.value = article.tags.join(", ");
+            setIsUpdate(true);
+            
+            console.log(isUpdate.current)
+        }
+        const id = searchParams.get("_id");
+        if (id) {
+            console.log(id);
+            loadArticle(id);
+        }
+    }, [searchParams]);
+
+    return (
+        <div
+            className={
+                "container md:shadow-lg max-w-screen-md mx-auto p-4 " +
+                className
+            }
+        >
+            <SendableForm
+                href="/api/v2/articles"
+                submitHandler={
+                    isUpdate === true ? (url, element, callback) => {
+                              patchData(
+                                  url + `?_id=${searchParams.get("_id")}`,
+                                  element,
+                                  callback,
+                                  {},
+                                  {
+                                      "x-access-token":
+                                          localStorage.getItem("accessToken"),
+                                  }
+                              );
+                          }
+                        : (url, element, callback) => {
+
+                              sendFormData(
+                                  url,
+                                  element,
+                                  callback,
+                                  { body: articleBody },
+                                  {
+                                      "x-access-token":
+                                          localStorage.getItem("accessToken"),
+                                  }
+                              );
+                          }
+                }
+            >
+                <CustomInput
+                    label="Title"
+                    id="title"
+                    reference={title}
+                    placeholder="Your title..."
+                    className="mb-4 text-2xl"
+                />
+                <RichTextEditor
+                    className="mb-4 min-h-500px"
+                    label="Article body"
+                    onChange={(e) => {
+                        setArticleBody(e);
+                    }}
+                />
+                <CustomTextArea
+                    className="mb-4"
+                    label="Short description"
+                    reference={description}
+                    id="description"
+                />
+                <CustomInput
+                    autoComplete="off"
+                    label="Tags"
+                    id="tags"
+                    reference={tags}
+                    placeholder="text"
+                    className="mb-4"
+                />
+                <TextButton>POST ARTICLE</TextButton>
+            </SendableForm>
+        </div>
+    );
 }
